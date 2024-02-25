a82447d7f664c2aed4e01de3b612c43705c482b6
doonpy <nguyenduypoon@gmail.com>
2024-02-25 10:42:07 +0700

    feat: remove prisma-uow

--- packages/prisma-uow/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/als.service';
-export * from './lib/constants';
-export * from './lib/prisma-unit-of-work.interceptor';
-export * from './lib/prisma-unit-of-work.module';
--- packages/prisma-uow/src/lib/constants.ts
+++
@@ -1,2 +0,0 @@
-export const PRISMA_SERVICE = Symbol('PRISMA_SERVICE');
-export const ALS_SERVICE = Symbol('ALS_SERVICE');
